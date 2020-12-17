c5e921e47bdb86c07a5abe880697f108d4c46fb7
Angelo <angelo@techdog.com.br>
2020-12-17 16:22:16 -0300

    ajuste

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let bodyParser = require("body-parser");
 let app = express();
 const port = require("dotenv");
 const server = require("http").createServer(app);
-const MongoClient = require("mongodb").MongoClient;
+const { MongoClient } = require("mongodb");
 const cors = require("cors");
 app.use(bodyParser.json());
 app.use(cors());
@@ -14,24 +14,73 @@ app.get("/", (req, res) => {
 });
 
 var database;
+async function connect() {
+  if (global.db) return global.db;
 
-// Connect to Mongoose and set connection variable
-MongoClient.connect(
-  "mongodb://conectedudemonstracao:0aHMcTOvR2Xu1kQpNoI9QHm7LB4qc7vsgQTRfsEBc9u4nT0fPnuq8v9UvSvDDUmwwifKi0Nt08rkBKOGNfaPkg==@conectedudemonstracao.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@conectedudemonstracao@&retrywrites=false",
-  function (err, client) {
-    console.log("Connected successfully to server");
-    const db = client.db("logsNiteroi");
-    database = db;
+  const conn = await MongoClient.connect(
+    "Ïmongodb://conectedudemonstracao:0aHMcTOvR2Xu1kQpNoI9QHm7LB4qc7vsgQTRfsEBc9u4nT0fPnuq8v9UvSvDDUmwwifKi0Nt08rkBKOGNfaPkg==@conectedudemonstracao.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@conectedudemonstracao@&retrywrites=false",
+    {
+      useUnifiedTopology: true,
+    }
+  );
+
+  if (!conn) return new Error("Falha na conexão");
+
+  global.db = await conn.db("logsNiteroi");
+
+  return global.db;
+}
+app.get("/faq", async (req, res, next) => {
+  try {
+    const db = await connect();
+    res.json(await db.collection("faq").find().toArray());
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
   }
-);
+});
+
+app.post("/postlog", async (req, res, next) => {
+  try {
+    const db = await connect();
+    db.collection("logs").insertOne(req.body);
+    res.json(await {status: 'Cadastrado'});
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
+  }
+});
+// Connect to Mongoose and set connection variable
+// MongoClient.connect(
+//   "mongodb://conectedudemonstracao:0aHMcTOvR2Xu1kQpNoI9QHm7LB4qc7vsgQTRfsEBc9u4nT0fPnuq8v9UvSvDDUmwwifKi0Nt08rkBKOGNfaPkg==@conectedudemonstracao.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@conectedudemonstracao@&retrywrites=false",
+//   function (err, client) {
+//     console.log("Connected successfully to server");
+//     const db = client.db("logsNiteroi");
+//     var data = db.collection("faq");
+//     console.log(data);
+//     database = db;
+//   }
+// );
 
 // var port = process.env.PORT || 8080;
 
 // Send message for default URL
+app.get("/123123", (req, res) => {
+  // const data = await database.collection("logs").find();
+  // .then(() => {})
+  // .catch((err) => console.log(err));
+  res.status(200).json(database.collection("logs").find());
+});
+// app.post("/postlog", (req, res) => {
+//   db.collection("logs")
+//     .insertOne(req.body)
+//     .then(() => {})
+//     .catch((err) => console.log(err));
+//   res.status(200).json({ status: "Cadastrado" });
+// });
 
-app.post("/postlog", (req, res) => {
-  database
-    .collection("logs")
+app.post("/postfaq", (req, res) => {
+  db.collection("faq")
     .insertOne(req.body)
     .then(() => {})
     .catch((err) => console.log(err));
